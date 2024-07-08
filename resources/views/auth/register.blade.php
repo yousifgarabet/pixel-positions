0bf270c77087455798a4a85d3b8bc263977219de
Yousif Arshak <yousifgarabet@gmail.com>
2024-07-08 14:55:46 +0200

    Log in and register created

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
     <x-page-heading>Register</x-page-heading>
+    <x-forms.form method="post" action='/register'>
+        <x-forms.input label="Your Name" name="name"/>
+
+    </x-forms.form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -9,3 +10,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [JobController::class, 'index']);
 
 Route::get('/register',[RegisteredUserController::class, 'create']);
+Route::post('/register',[RegisteredUserController::class, 'store']);
+
+Route::get('/login',[SessionController::class, 'create']);
+Route::post('/login',[SessionController::class, 'store']);
+Route::delete('/login',[SessionController::class, 'destory']);
+
