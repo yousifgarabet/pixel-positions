cd85a5cc1d4d91b1800277cde03211e6f10c11ea
Yousif Arshak <yousifgarabet@gmail.com>
2024-07-08 14:59:16 +0200

    add group to auth

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
 
-Route::middleware('guest', function() {
+Route::middleware('guest')->group( function() {
     Route::get('/register',[RegisteredUserController::class, 'create']);
     Route::post('/register',[RegisteredUserController::class, 'store']);
     Route::get('/login',[SessionController::class, 'create']);
